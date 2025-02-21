ebd8bacfcd1292e7ca05d894d0ec8a8061ef0f97
ShivamRathaurIITH <rathoreshivam.iith@gmail.com>
2025-02-21 14:15:09 +0530

    python programming repo

---
+++ test.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+import sys 
+print(sys.version) # version of python
+
+# python file_name.py - to run the program
+
+# `python` command to initiate python environment in cmd
+# exit() to exit from the python environment created in cmd
